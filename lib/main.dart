40ce9bca7d53d221066261df8d94595b8b7f0c27
WC Leung <lwchkg@gmail.com>
2023-02-09 01:22:58 +0000

    lib/main.dart: reorder import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:intl/intl_standalone.dart'
     if (dart.library.html) 'package:intl/intl_browser.dart';
 import 'package:timezone/data/latest.dart' as tz;
