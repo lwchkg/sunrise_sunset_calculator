df3444504c9002e477c72bebb6add5931e596a0c
WC Leung <lwchkg@gmail.com>
2023-09-19 13:23:47 +0100

    Fix typo

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -30,7 +30,7 @@ Future<Position> _determinePosition() async {
   if (permission == LocationPermission.denied) {
     permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
-      return Future.error('Location permissions are denied');
+      return Future.error('Location permissions are denied.');
     }
   }
 
